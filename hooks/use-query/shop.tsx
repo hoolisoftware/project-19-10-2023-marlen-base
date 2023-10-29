d0798a624fa54d9bd7fa8f44257bcac98af0ebdc
LasterAlex <sasha.ishukshin@gmail.com>
2023-10-29 14:51:30 +0200

    Added shop hooks and shop items display

---
+++ hooks/use-query/shop.tsx
@@ -0,0 +1,22 @@
+import useBaseQuery from ".";
+import type { APIResponse } from '@/types/api'
+import type { Model } from "@/types/models";
+
+
+export interface ShopItem extends Model {
+    id: number
+    name: string
+    description: string
+    photo_url: string
+    type: string
+    crystals: number
+    price: number
+    sgd_price: number
+    for_sale: boolean
+    cashback: number
+}
+
+
+export function useShopItems() {
+    return useBaseQuery<APIResponse<{items: ShopItem[]}>>(['items'], `payments/items/`)
+}
--- pages/shop.vue
+++ pages/shop.vue
@@ -1,17 +1,34 @@
+<script setup lang="ts">
+import { useShopItems } from "~/hooks/use-query/shop";
+
+const {data: itemsData, isLoading: itemsIsLoading, isError: itemsIsError} = useShopItems()
+</script>
+
+
 <template>
   <div class="page">
     <title-section text="Магазин"/>
-    <div class="shop">
-      <shop-card title="Предмет" image="/img/cases/case_1.png" :cost="200"/>
-      <shop-card title="Предмет" image="/img/cases/case_1.png" :cost="200"/>
-      <shop-card title="Предмет" image="/img/cases/case_1.png" :cost="200"/>
+    <div v-if="itemsIsLoading">
+        Loading...
+    </div>
+    <div v-else-if="itemsIsError">
+        Error...
+    </div>
+    <div v-else="itemsData?.data">
+        <div class="shop">
+          <div v-for="item in itemsData?.data.items">
+            <shop-card :title="item.name" :image="SERVER_URL + item.photo_url" :cost="item.price"/>
+          </div> 
+        </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import shopCard from "@/components/cards/shop-card.vue";
 import titleSection from "@/components/blocks/title-section.vue";
+import { SERVER_URL } from "~/config";
+
 export default {
   name: "shop",
   head: {
