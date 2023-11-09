d287749578cfbaaca077dc3190ef254fe15af446
LasterAlex <sasha.ishukshin@gmail.com>
2023-11-09 17:02:53 +0200

    Cleaned up

--- components/misc/roulette-items.vue
+++ components/misc/roulette-items.vue
@@ -72,6 +72,7 @@ export default {
             this.clicked = false;
             this.instantly = false;
             const wait_before_again = 100;
+
             this.items.forEach(async (item) => {
                 const element = document.getElementById(`el${this.items.indexOf(item)}`);
                 element.style.opacity = ""
@@ -80,18 +81,21 @@ export default {
                     element.style.transform = `translateX(${(this.items.length-this.bufferItems*2)/2*100-50}%)`;
                     await new Promise(r => setTimeout(r, wait_before_again));
                 }
-                element.style.transition = "transform 8s cubic-bezier(.08,.67,.25,1)"
+                element.style.transition = ""
                 element.style.transform = `translateX(${(-(this.items.length-this.bufferItems*2)/2*100+(10+this.random_num*80))}%)`;
             }); 
+
             if (this.rouletteStores.animationState === 'again') {
                 await new Promise(r => setTimeout(r, wait_before_again));
             }
+
             const animationStartTime = Date.now();
             let currentTime = Date.now();
             let elapsed = currentTime - animationStartTime;
             let animationCompletion = 0;
             let newAnimationCompletion = 0;
             let bezierCurve = BezierEasing(.08,.67,.25,1)  //.05,.79,.25,1   .25, .1,.25, 1
+
             while (elapsed < this.animationDuration) {
                 currentTime = Date.now();
                 elapsed = currentTime - animationStartTime;
@@ -102,6 +106,7 @@ export default {
                 this.clicked = Math.floor((this.items.length-this.bufferItems*2-(10+this.random_num*80)/100-0.5)*animationCompletion+0.5)+this.bufferItems;
                 await new Promise(r => setTimeout(r, 30));
             }
+            
             this.clicked = false;
             this.rouletteStores.animationState = 'ended';
             this.items.forEach(async (item) => {
