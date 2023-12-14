045ff03b70c43020ed37b6471746230e8e0f5414
LasterAlex <sasha.ishukshin@gmail.com>
2023-12-14 13:56:33 +0200

    Made user profile better

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -21,37 +21,7 @@ const {data: inventoryData, isLoading: inventoryIsLoading, isError: inventoryIsE
                         <img v-if="profileData?.data.user.photo_url" :src="profileData?.data.user.photo_url" alt="Аватар"/>
                         <nuxt-img v-else src="/img/avatars/no-avatar.png" format="webp" class="profile-avatar"/>
                     </div>
-                    <div class="profile-name"> {{ profileData?.data.user.first_name + (profileData?.data.user.last_name ? profileData?.data.user.last_name : "") }} </div>
-                    <!-- <div class="profile-contacts" v-if="$route.params.otherProfile">
-                        <div>
-                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_214_6157)">
-                                    <path d="M0 15.3333C0 8.10512 0 4.491 2.2455 2.2455C4.491 0 8.10512 0 15.3333 0H16.6666C23.8948 0 27.5089 0 29.7544 2.2455C32 4.491 32 8.10512 32 15.3333V16.6666C32 23.8948 32 27.5089 29.7544 29.7544C27.509 32 23.8949 32 16.6667 32H15.3334C8.10519 32 4.49106 32 2.24556 29.7545C0 27.509 0 23.8949 0 16.6667V15.3333Z" fill="#2787F5"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.66727 10H6.33339C5.66658 10 5.5332 10.3139 5.5332 10.6599C5.5332 11.2779 6.32445 14.3432 9.21733 18.3972C11.146 21.1659 13.8631 22.6666 16.3357 22.6666C17.8193 22.6666 18.0028 22.3333 18.0028 21.7591V19.6666C18.0028 18.9999 18.1433 18.8669 18.613 18.8669C18.9592 18.8669 19.5526 19.0399 20.9373 20.3748C22.5197 21.9569 22.7806 22.6666 23.6706 22.6666H26.0045C26.6714 22.6666 27.0048 22.3333 26.8125 21.6755C26.602 21.0199 25.8465 20.0686 24.8439 18.9411C24.2999 18.2984 23.484 17.6062 23.2367 17.2601C22.8906 16.8152 22.9895 16.6174 23.2367 16.2219C23.2367 16.2219 26.0802 12.2172 26.3769 10.8577C26.5252 10.3633 26.3769 10 25.6711 10H23.3372C22.7438 10 22.4702 10.3139 22.3218 10.6599C22.3218 10.6599 21.135 13.5521 19.4537 15.4309C18.9097 15.9747 18.6625 16.1478 18.3657 16.1478C18.2174 16.1478 18.0026 15.9747 18.0026 15.4804V10.8577C18.0026 10.2644 17.8305 10 17.3358 10H13.6683C13.2975 10 13.0745 10.2753 13.0745 10.5363C13.0745 11.0987 13.9151 11.2284 14.0018 12.8105V16.2466C14.0018 16.9999 13.8656 17.1366 13.569 17.1366C12.7778 17.1366 10.8531 14.2314 9.7117 10.9071C9.48814 10.261 9.26377 10 8.66727 10Z" fill="white"/>
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_214_6157">
-                                        <rect width="32" height="32" fill="white"/>
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                        </div>
-                        <div>
-                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_214_6161)">
-                                    <path d="M0 15.3333C0 8.10512 0 4.491 2.2455 2.2455C4.491 0 8.10512 0 15.3333 0H16.6666C23.8948 0 27.5089 0 29.7544 2.2455C32 4.491 32 8.10512 32 15.3333V16.6666C32 23.8948 32 27.5089 29.7544 29.7544C27.509 32 23.8949 32 16.6667 32H15.3334C8.10519 32 4.49106 32 2.24556 29.7545C0 27.509 0 23.8949 0 16.6667V15.3333Z" fill="#26A5E4"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.24335 15.8309C11.9077 13.7988 15.018 12.459 16.5742 11.8118C21.0175 9.96363 21.9409 9.64257 22.5426 9.63195C22.675 9.62963 22.9709 9.66245 23.1626 9.81801C23.447 10.0488 23.4486 10.5496 23.417 10.8811C23.1762 13.4111 22.1344 19.5507 21.6043 22.3843C21.38 23.5833 20.9384 23.9854 20.5109 24.0247C19.5817 24.1102 18.8762 23.4106 17.9762 22.8208C16.568 21.8976 15.7725 21.323 14.4055 20.4223C12.8259 19.3813 13.8499 18.8091 14.7502 17.8741C14.9858 17.6294 19.0796 13.9057 19.1589 13.5679C19.1688 13.5256 19.178 13.3681 19.0844 13.285C18.9909 13.2019 18.8528 13.2303 18.7532 13.2529C18.6119 13.2849 16.3623 14.7719 12.0043 17.7136C11.3657 18.1521 10.7874 18.3658 10.2692 18.3546C9.69785 18.3423 8.59898 18.0316 7.78204 17.766C6.78004 17.4403 5.98367 17.2681 6.05304 16.7149C6.08917 16.4268 6.48592 16.1321 7.24335 15.8309Z" fill="white"/>
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_214_6161">
-                                        <rect width="32" height="32" fill="white"/>
-                                    </clipPath>
-                                </defs>
-                            </svg>
-
-                        </div>
-                    </div> -->
-                    
+                    <div class="profile-name"> {{ profileData?.data.user.first_name + (profileData?.data.user.last_name ? profileData?.data.user.last_name : "") }} </div>                    
                     <div v-if="statsIsLoading">
                         Loading...
                     </div>
@@ -88,8 +58,8 @@ const {data: inventoryData, isLoading: inventoryIsLoading, isError: inventoryIsE
                         <div class="inventory-top">
                             <div class="inventory-top_title">
                                 <h2>Инвентарь</h2>
-                                <span>{{ inventoryData?.data.items.filter((item) => activeTab === 0 || (item.is_ordered && activeTab === 1)).length }} 
-                                  {{ getNoun(inventoryData?.data.items.filter((item) => activeTab === 0 || (item.is_ordered && activeTab === 1)).length, "предмет", "предмета", "предметов") }}</span>
+                                <span>{{ inventoryData?.data.items.filter((item) => (!item.is_ordered && activeTab === 0 && !item.is_sold) || (item.is_ordered && activeTab === 1 && !item.is_sold)).length }} 
+                                  {{ getNoun(inventoryData?.data.items.filter((item) => (!item.is_ordered && activeTab === 0 && !item.is_sold) || (item.is_ordered && activeTab === 1 && !item.is_sold)).length, "предмет", "предмета", "предметов") }}</span>
                             </div>
                         </div>
                         <div class="inventory-tabs">
@@ -98,8 +68,8 @@ const {data: inventoryData, isLoading: inventoryIsLoading, isError: inventoryIsE
                             <div :class="'inventory-tabs_item' + (activeTab === 1 ? ' inventory-tabs_item__active':'')" v-on:click="activeTab = 1">Выведено</div>
                         </div>
                         <div class="inventory-items">
-                          <div v-for="profile_item in inventoryData?.data.items.filter((item) => activeTab === 0 || (item.is_ordered && activeTab === 1))">
-                            <profile-item :image="SERVER_URL + profile_item.item.photo_url"/>
+                          <div v-for="profile_item in inventoryData?.data.items.filter((item) => (!item.is_ordered && activeTab === 0 && !item.is_sold) || (item.is_ordered && activeTab === 1 && !item.is_sold))">
+                            <profile-item :image="SERVER_URL + profile_item.item.photo_url" :cost="profile_item.item.price"/>
                           </div>
                         </div>
                     </div>
@@ -182,6 +152,7 @@ export default {
       width: 100%;
       height: 100%;
       border-radius: 120px;
+      object-fit: cover;
     }
   }
   &-name {
@@ -288,13 +259,31 @@ export default {
   }
 }
 
+::-webkit-scrollbar {
+    width: 3px;
+}
+
+::-webkit-scrollbar-track {
+    background: var(--scroll-track-color);
+}
+
+::-webkit-scrollbar-thumb {
+    background: var(--scroll-thumb-color);
+    border-radius: 5px;
+    min-height: 20px !important;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: var(--scroll-thumb-hovered-color);
+}
+
 
 .inventory {
   display: flex;
   flex-direction: column;
   width: 644px;
   height: max-content;
-  max-height: 420px;
+  height: 420px;
   box-sizing: border-box;
   padding: 20px;
   gap: 15px;
@@ -404,6 +393,7 @@ export default {
     flex-wrap: wrap;
     gap: 10px;
     overflow: auto;
+      scrollbar-color: var(--scroll-thumb-color) var(--scroll-track-color);
       box-sizing: border-box;
       padding-right: 5px;
       &::-webkit-scrollbar {
